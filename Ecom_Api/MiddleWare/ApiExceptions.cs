1d84c2dd2eb8b983a705c36bd0ee2bb6f1275f76
NAIEF-2004 <zeenteeyt@gmail.com>
2025-10-28 18:30:01 +0300

    add-middleware to securet

---
+++ Ecom_Api/MiddleWare/ApiExceptions.cs
@@ -0,0 +1,13 @@
+﻿using Ecom_Api.Helper;
+
+namespace Ecom_Api.MiddleWare
+{
+    public class ApiExceptions : ResponseAPI
+    {
+        public ApiExceptions(int statusCod, string message = null, string Details=null) : base(statusCod, message)
+        {
+           this.Details = Details;
+        }
+        public string Details { get; set; }
+    }
+}
---
+++ Ecom_Api/MiddleWare/ExceptionsMiddleWare.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Ecom_Api.MiddleWare
+{
+
+    //ان الفائدة من المدل وير هو عبارة عن بوابة لا يدخل الطلب مباشر للكنترولر وانما يمر عليها اولا
+    public class ExceptionsMiddleWare
+    {
+        private readonly RequestDelegate next;
+
+        public ExceptionsMiddleWare(RequestDelegate next)
+        {
+            this.next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                context.Response.ContentType = "application/json";
+                var response = new ApiExceptions((int)HttpStatusCode.InternalServerError,ex.Message,ex.StackTrace);
+                var json=JsonSerializer.Serialize(response);
+                await context.Response.WriteAsync(json);
+            }
+        }
+    }
+}
